cce376811595ee2ce8fdf8edec268b52a2543f4d
MathisSTL <141674101+MathisSTL@users.noreply.github.com>
2023-08-14 14:56:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,52 @@
-const board = document.getElementById("board");
-const shuffleButton = document.getElementById("shuffleButton");
-
-const initialState = [1, 2, 3, 4, 5, 6, 7, 8, ""];
-const winningState = [...initialState];
-
-let tiles = [];
-let emptyIndex = initialState.indexOf("");
-
-function initializeTiles() {
-  for (let i = 0; i < initialState.length; i++) {
-    const tile = document.createElement("div");
-    tile.className = "tile";
-    tile.textContent = initialState[i] !== "" ? initialState[i] : "";
-    tiles.push(tile);
-    tile.addEventListener("click", () => handleTileClick(i));
-    board.appendChild(tile);
-  }
+function shuffle() {
+    var grid = document.getElementById('game');
+    for(var i = grid.children.length; i >= 0; i--) {
+        grid.appendChild(grid.children[Math.random() * i | 0]);
+    }
 }
 
-function shuffleTiles() {
-  for (let i = tiles.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [tiles[i], tiles[j]] = [tiles[j], tiles[i]];
-  }
-  emptyIndex = tiles.findIndex(tile => tile.textContent === "");
-  updateTiles();
+function loadGame() {
+    shuffle();
 }
 
-function updateTiles() {
-  tiles.forEach((tile, index) => {
-    tile.textContent = initialState[index] !== "" ? initialState[index] : "";
-  });
+window.addEventListener('click', function (e) {
+
+    //On est sur un Ã©lÃ©ment de classe item (un chiffre)
+    if(e.target.className === 'item') {
+        var emptyItem = document.querySelector('.empty');
+
+        if(getDistance(e.target.offsetLeft, e.target.offsetTop, emptyItem.offsetLeft, emptyItem.offsetTop) <= 110)
+        {
+            //On doit intervertir le vide et le chiffre
+            emptyItem.className = 'item';
+            emptyItem.innerText = e.target.innerText;
+            e.target.className = 'empty';
+            e.target.innerText = '';
+
+            checkVictory();
+        }
+    }
+
+});
+
+function getDistance(x1, y1, x2, y2) {
+    var a = x1 - x2;
+    var b = y1 - y2;
+    return Math.sqrt(a * a + b * b);
 }
 
-function handleTileClick(clickedIndex) {
-  if (isValidMove(clickedIndex)) {
-    swapTiles(clickedIndex, emptyIndex);
-    emptyIndex = clickedIndex;
-    updateTiles();
-    checkWin();
-  }
+function checkVictory() {
+
+    var items = document.querySelectorAll('#game>div');
+    var score = 0;
+    for(var i = 0; i < items.length; i++) {
+        if(items[i].innerText === ('' + (i + 1))) {
+            score++;
+        }
+    }
+
+    if(score >= 15) {
+        var victoryItem = document.querySelector('.victory');
+        victoryItem.style.opacity = "1";
+    }
 }
-
-function isValidMove(clickedIndex) {
-  const rowDiff = Math.abs(clickedIndex % 3 - emptyIndex % 3);
-  const colDiff = Math.abs(Math.floor(clickedIndex / 3) - Math.floor(emptyIndex / 3));
-  return (rowDiff === 1 && colDiff === 0) || (rowDiff === 0 && colDiff === 1);
-}
-
-function swapTiles(index1, index2) {
-  [initialState[index1], initialState[index2]] = [initialState[index2], initialState[index1]];
-}
-
-function checkWin() {
-  if (JSON.stringify(initialState) === JSON.stringify(winningState)) {
-    alert("Vous avez gagné !");
-  }
-}
-
-initializeTiles();
-shuffleTiles();
-
-shuffleButton.addEventListener("click", shuffleTiles);
